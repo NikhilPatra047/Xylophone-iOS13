4a40522a4f429de4c2b8c8f73848aa6c5c157bda
NikhilPatra047 <patra.nikhil04@gmail.com>
2021-08-07 15:51:58 +0530

    Xylophone app
    Created a xylophone app. By creating this I learnt about, delaying my code using DispatchQueue, functions, parametric functions, and playing music in the app using AVFoundation.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print(sender.currentTitle!) //sender is basically the identity of the buttton.
+        sender.alpha = 0.5
+        playSound(soundName: sender.currentTitle!) //! Force-unwrap means it sender will return the currentTitle especially if the button has a title to it. Button without title won't be returned.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
+
 
